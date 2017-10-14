d88fbe09750efccf8bd74668553060230c445019
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-10-15 02:41:09 +0300

    Kendo added! User ads with delete functionality added.

--- FourWheel/FourWheels.Web/App_Start/BundleConfig.cs
+++ FourWheel/FourWheels.Web/App_Start/BundleConfig.cs
@@ -8,8 +8,16 @@ namespace FourWheels.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/Kendo/jquery.min.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/unobtrusiveJQ").Include(
+                        "~/Scripts/Kendo/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/unobtrusiveJQ").Include(
+                     "~/Scripts/jquery.unobtrusive-ajax.min.js"));
+
+
+            // jquery.unobtrusive-ajax.min
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -17,12 +25,27 @@ namespace FourWheels.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                      "~/Scripts/Kendo/kendo.web.min.js",
+                      "~/Scripts/Kendo/kendo.aspnetmvc.min.js"));
+
+
+
+
+
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/basicAad").Include(
                       "~/Content/basic-ad.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendoCss").Include(
+                      "~/Content/Kendo/kendo.bootstrap.min.css",
+                      "~/Content/Kendo/kendo.common.min.css"));
+
+
         }
     }
 }
