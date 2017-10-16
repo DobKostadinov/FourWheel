d5f76ccd074cae121d070a91d9f309622a081cdc
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-10-16 03:15:21 +0300

    CarFeatureServicesTests added.

---
+++ FourWheel/FourWheels.Tests/Services/CarFeatureServicesTests/Constructor_Should.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using FourWheels.Services;
+using FourWheels.Data.Models;
+using FourWheels.Data.Repositories;
+using FourWheels.Services.Contracts;
+
+using NUnit.Framework;
+using Moq;
+
+namespace FourWheels.Tests.Services.CarFeatureServicesTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        private IEfRepostory<CarFeature> carFeatureRepo;
+
+        [SetUp]
+        public void Init()
+        {
+            this.carFeatureRepo = new Mock<IEfRepostory<CarFeature>>().Object;
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenRepositoryIsNull()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new CarFeaturesServices(null));
+        }
+
+        [Test]
+        public void NotThrow_WhenEverythingIsPassed()
+        {
+            // Act & Assert
+            Assert.DoesNotThrow(() => new CarFeaturesServices(this.carFeatureRepo));
+        }
+
+        [Test]
+        public void ReturnCarFeatureServiceInstance_WhenCorrectDataIsPassed()
+        {
+            // Act
+            var newCarFeatureServiceInstance = new CarFeaturesServices(this.carFeatureRepo);
+
+            // Assert
+            Assert.IsInstanceOf<ICarFeatureServices>(newCarFeatureServiceInstance);
+        }
+    }
+}
