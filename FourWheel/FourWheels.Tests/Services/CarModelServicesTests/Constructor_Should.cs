bb25c76394fe3285978d8d560e6a29777f4ee84e
Dob Kostadinov <dobromir.j.kostadinov@gmail.com>
2017-10-16 03:15:43 +0300

    CarModelServicesTests added.

---
+++ FourWheel/FourWheels.Tests/Services/CarModelServicesTests/Constructor_Should.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using FourWheels.Services;
+using FourWheels.Data.Models;
+using FourWheels.Data.Repositories;
+using FourWheels.Services.Contracts;
+
+using NUnit.Framework;
+using Moq;
+
+namespace FourWheels.Tests.Services.CarModelServicesTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        private IEfRepostory<CarModel> carModelRepo;
+
+        [SetUp]
+        public void Init()
+        {
+            this.carModelRepo = new Mock<IEfRepostory<CarModel>>().Object;
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenRepositoryIsNull()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new CarModelServices(null));
+        }
+
+        [Test]
+        public void NotThrow_WhenEverythingIsPassed()
+        {
+            // Act & Assert
+            Assert.DoesNotThrow(() => new CarModelServices(this.carModelRepo));
+        }
+
+        [Test]
+        public void ReturnCarModelServicesInstance_WhenCorrectDataIsPassed()
+        {
+            // Act
+            var newCarFeatureServicesInstance = new CarModelServices(this.carModelRepo);
+
+            // Assert
+            Assert.IsInstanceOf<ICarModelServices>(newCarFeatureServicesInstance);
+        }
+    }
+}
---
+++ FourWheel/FourWheels.Tests/Services/CarModelServicesTests/GetAllCarModels_Should.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using FourWheels.Data.Models;
+using FourWheels.Data.Repositories;
+using FourWheels.Services;
+
+using Moq;
+using NUnit.Framework;
+
+namespace FourWheels.Tests.Services.CarModelServicesTests
+{
+    [TestFixture]
+    public class GetAllCarModels_Should
+    {
+        private Mock<IEfRepostory<CarModel>> carModelsRepoMocked;
+        private IQueryable<CarModel> carModels;
+
+        [SetUp]
+        public void Init()
+        {
+            this.carModelsRepoMocked = new Mock<IEfRepostory<CarModel>>();
+
+            this.carModels = new List<CarModel>
+            {
+                new CarModel { Model = "A99" },
+                new CarModel { Model = "Y6" },
+                new CarModel { Model = "Ugly" },
+            }.AsQueryable();
+
+            carModelsRepoMocked.Setup(x => x.All).Returns(carModels);
+        }
+
+        [Test]
+        public void ReturnQueryable_WithExactNumberOfCarModels()
+        {
+            // Arrange
+            var carModelsServices = new CarModelServices(this.carModelsRepoMocked.Object);
+
+            // Act 
+            var expectedNumberOfCarModels = carModelsServices.GetAllCarModels().Count();
+
+            // Assert
+            Assert.AreEqual(expectedNumberOfCarModels, this.carModels.Count());
+        }
+
+        [Test]
+        public void ReturnInstanceOfEnumerable_WithValidaDataIsPassed()
+        {
+            // Arrange
+            var carModelsServices = new CarModelServices(this.carModelsRepoMocked.Object);
+
+            // Act 
+            var allCarsResult = carModelsServices.GetAllCarModels();
+
+            // Assert
+            Assert.IsInstanceOf<IEnumerable<CarModel>>(allCarsResult);
+        }
+
+        [Test]
+        public void Call_AllMethodFromRepositoryOnce()
+        {
+            // Arrange
+            var carModelServices = new CarModelServices(this.carModelsRepoMocked.Object);
+
+            // Act 
+            carModelServices.GetAllCarModels();
+
+            // Assert
+            this.carModelsRepoMocked.Verify(x => x.All, Times.Once);
+        }
+    }
+}
